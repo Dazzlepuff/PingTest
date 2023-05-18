c69cc30ca56447aef70a5e6aa654efab1121082b
Dazzlepuff <89800231+Dazzlepuff@users.noreply.github.com>
2023-05-18 10:52:43 -0500

    Add files via upload

---
+++ TerminalPingTest/CreatePingTest.bat
@@ -0,0 +1,33 @@
+@echo off
+
+mkdir "C:\RITE\TerminalPingTest\PingLogs"
+
+for /f "tokens=3" %%G in ('route print ^| findstr "\<0.0.0.0\>"') do set GATEWAY=%%G
+set TERMINALIP=192.168.0.125
+
+for /f "tokens=1-4 delims=:/ " %%A in ("%DATE% %TIME%") do (
+    set TIMESTAMP=%%D_%%B-%%C
+)
+
+for /f "tokens=1-2 delims=: " %%E in ('time /t') do (
+    set TIMESTAMP=%TIMESTAMP%_%%E-%%F
+)
+
+set LOGFILE=C:\RITE\TerminalPingTest\PingLogs\pinglog_%TIMESTAMP%.txt
+
+echo Starting ping test to %GATEWAY% at %TIME% on %DATE% >> %LOGFILE%
+echo Starting ping test to %TERMINALIP% at %TIME% on %DATE% >> %LOGFILE%
+
+:LOOP
+ping %GATEWAY% -n 1 -w 1000 | find "TTL=" > nul
+if errorlevel 1 (
+  echo Ping to %GATEWAY% failed at %TIME% on %DATE% >> %LOGFILE%
+)
+ping -n 5 127.0.0.1 > nul
+
+ping %TERMINALIP% -n 1 -w 1000 | find "TTL=" > nul
+if errorlevel 1 (
+  echo Ping to %TERMINALIP% failed at %TIME% on %DATE% >> %LOGFILE%
+)
+ping -n 5 127.0.0.1 > nul
+goto LOOP
---
+++ TerminalPingTest/RunPingTest.vbs
@@ -0,0 +1,1 @@
+CreateObject("Wscript.Shell").Run "cmd /c ""C:\RITE\TerminalPingTest\CreatePingTest.bat""", 0, False
